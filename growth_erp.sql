739306d4bf3db34e2b02a5a8a907d75893149dc1
My Name <myemail@example.com>
2024-11-15 18:14:26 +0400

    chages

--- growth_erp.sql
+++ growth_erp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 12, 2024 at 04:46 PM
+-- Generation Time: Nov 15, 2024 at 03:13 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 7.4.33
 
